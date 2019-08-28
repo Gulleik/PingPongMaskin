e8d0aaccd9c9eb52c86c93b524617d9f87c3f55f
Gulleik <gulleik.olsen@gmail.com>
2019-08-28 14:47:56 +0200

    recieve and initialize

--- UART_driver.c
+++ UART_driver.c
@@ -1,9 +1,35 @@
 #include <UART_driver.h>
 
-void UART_driver_receive_data() {
+#define FOSC 4915200// Clock Speed
+#define BAUD 9600
 
+unsigned char UART_driver_receive_data() {
+		/* Wait for data to be received */
+		while (!(UCSRA & (1 << RXC)));
+		/* Get and return received data from buffer */
+		return UDR;
 }
 
 void UART_driver_transmit_data() {
     
 }
+
+
+void UART_driver_initialize() {
+	UBRRH = (unsigned char)((FOSC / 16 / BAUD - 1) >> 8);
+	UBRRL = (unsigned char)(FOSC / 16 / BAUD - 1);
+	/* Enable receiver and transmitter */
+	UCSRB = (1 << RXEN) | (1 << TXEN);
+	/* Set frame format: 8data, 2stop bit */
+	UCSRC = (1 << URSEL) | (1 << USBS) | (3 << UCSZ0);
+}
+
+void main(void)
+{
+	...
+		USART_Init(MYUBRR);
+	...
+}
+void USART_Init(unsigned int ubrr)
+{
+	
--- UART_driver.h
+++ UART_driver.h
@@ -1,6 +1,8 @@
 #include <avr/io.h>
 #include <driver
 
+void UART_driver_initialize();
+
 void UART_driver_receive_data();
 
 void UART_driver_transmit_data();
