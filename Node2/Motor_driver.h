2262bc79a67bab488fb04b889abd6422a3bfefe8
Gulleik <learntoace@gmail.com>
2019-10-28 17:30:19 +0100

    enable motor_driver

--- Node2/Motor_driver.h
+++ Node2/Motor_driver.h
@@ -1,3 +1,9 @@
 
 
-void motor_driver_set_dir(uint8_t X);
+void motor_driver_set_dir(uint8_t joy_pos);
+
+void motor_driver_enable_motor();
+
+void motor_driver_disable_motor();
+
+void motor_driver_run(uint8_t joy_pos);
--- Node2/motor_driver.c
+++ Node2/motor_driver.c
@@ -1,6 +1,30 @@
 #include "Motor_driver.h"
+#include <avr/io.h>
 
+void motor_driver_set_dir(uint8_t joy_pos){
+    DDRH = 1;
+    if (joy_pos > 130){
+        PORTH |= (1 << PH1);
+    }else{
+        PORTH |= (0 << PH1);
+    }
+}
 
-void motor_driver_set_dir(uint8_t x){
-    
+void motor_driver_enable_motor(){
+    DDRH = 1;
+    PORTH |= (1 << PH4);
+}
+
+void motor_driver_disable_motor(){
+    DDRH = 1;
+    PORTH |= (1 << PH4);
+}
+
+void motor_driver_run(uint8_t joy_pos){
+    if( joy_pos != 130 && joy_pos != 131){
+        motor_driver_set_dir(uint8_t joy_pos)
+        motor_driver_enable_motor();
+    }else{
+        motor_driver_disable_motor();
+    }
 }
